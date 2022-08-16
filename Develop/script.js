669bf850476144e8b25fb6fb58314a7e165e9f3f
GH0ST <adamjamessherrill@gmail.com>
2022-08-16 10:02:13 -0500

    started funstion with characters variable with objects

--- Develop/script.js
+++ Develop/script.js
@@ -6,9 +6,36 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); // generatePassword is an empty function, in this function is where magic happens
   var passwordText = document.querySelector("#password");
+  
+  // My code
+  function generatePassword() {
+    var characters = {
+      lowerChars: 'abcdefghijklmnopqrstuvwxyz',
+      upperChars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      numberChars: '0123456789',
+      specialsChars: '!@#$%^&*()'
+    }
+    
+    window.prompt('How many characters would you like your password to contain?');
+    if(String.length > 8 && String.length < 128) {
+      window.alert('Click OK to confirm special characters')
+    } else return;
+    
 
+
+
+
+  
+  
+  
+  
+  
+  
+  }
+  
+  // Displays password on screen
   passwordText.value = password;
 
 }
