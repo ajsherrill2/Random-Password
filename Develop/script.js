8dcd5a0e6b6fe99b94fe2c9f34d8d4afe6d0b9d0
GH0ST <adamjamessherrill@gmail.com>
2022-08-16 17:35:47 -0500

    changed variables to a var with key-values, created empty strings for criteria to be added to and created if conditional statements

--- Develop/script.js
+++ Develop/script.js
@@ -15,26 +15,50 @@ function generatePassword() {
   console.log('Clicked button');
 
   //Password criteria
-  var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-  var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var numbers = '1234567890';
-  var specials = '!@#$%^&*()';
+  const allCharacters = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numbers: '1234567890',
+    specials: '!@#$%^&*()'
+  };
 
+  var combinedChars = "";
+  var generatedPass = "";
+
+  var passwordLength = window.prompt('How many characters would you like your password to contain?');
+    if (passwordLength >= 8 && passwordLength <= 128) {
+      window.confirm('Continue to characters types');
+    } else if (passwordLength > 128 || passwordLength < 8) {
+      window.alert('Please enter a number between 8 and 128');
+      return null;
+    } else {
+      window.alert('please make sure your value is number');
+    }
+
+  //
+  var addLowercaseChars = window.confirm('Click OK to confirm using lowercase characters');
+    if (addLowercaseChars) {
+      combinedChars += allCharacters.lowercase;  
+    }
+
+  //
+  var addUppercaseChars = window.confirm('Click OK to confirm using uppercase characters');
+    if (addUppercaseChars) {
+      combinedChars += allCharacters.uppercase;
+    }
   
-
-
-
-
-
-
-
-
-
-
-
+  //
+  var addNumericalChars = window.confirm('Click OK to confirm using numerical characters');  
+    if (addNumericalChars) {
+      combinedChars += allCharacters.numbers
+    }
   
-  // Generated password will go here
-  return 'Generated password here';
+  //
+  var addSpecialChars = window.confirm('Click OK to confirm using special characters');
+    if (addSpecialChars) {
+      combinedChars += allCharacters.specials
+    }    
+  
 }
 
 // Add event listener to generate button
